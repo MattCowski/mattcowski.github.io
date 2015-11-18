0adeb001ad6710badec582e52add725f9bebd1c1
MattCowski <MattCowski@gmail.com>
2015-11-18 22:42:24 +0000

    added images, css

---
+++ app.js
@@ -0,0 +1,8 @@
+var mountNode = document.getElementById('react')
+var HelloMessage = React.createClass({
+  render: function() {
+    return <div>Hello {this.props.name}</div>;
+  }
+});
+
+ReactDOM.render(<HelloMessage name="John" />, mountNode);
